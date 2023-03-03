50f47d0da26b59e00ed570ca60d9b9e3237d18fd
Tauane Sales <62819445+tauanesales@users.noreply.github.com>
2023-03-03 03:30:03 +0000

    [ADD] [Task 03] Add keys

--- App/src/components/Teclado.jsx
+++ App/src/components/Teclado.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import TeclasLinha1 from "./TeclasLinha1";
+import TeclasLinha2 from "./TeclasLinha2";
+import TeclasLinha3 from "./TeclasLinha3";
+
 
 function Teclado() {
-    return <div></div>;
+    return <div className="teclado coluna">
+        <TeclasLinha1 />
+        <TeclasLinha2 />
+        <TeclasLinha3 />
+    </div>;
 }
 
 export default Teclado;
